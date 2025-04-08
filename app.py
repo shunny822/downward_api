efd65ea81aa5a843ed3436297eba3728efcef31b
suhyun <sw92522@naver.com>
2025-04-08 16:58:23 +0900

    update app.py

--- app.py
+++ app.py
@@ -9,10 +9,9 @@ def info():
     node_name = os.getenv("NODE_NAME", "Unknown")
     pod_namespace = os.getenv("POD_NAMESPACE", "Unknown")
     return (
-        f"<h2>Downward API Example</h2>"
-        f"<p><strong>Pod Name:</strong> {pod_name}</p>"
-        f"<p><strong>Node Name:</strong> {node_name}</p>"
-        f"<p><strong>Namespace:</strong> {pod_namespace}</p>"
+        f"Container EDU | POD Working : {pod_name}</p>"
+        f" | nodename : {node_name}</p>"
+        f" | namespace : {pod_namespace}</p>"
     )
 
 if __name__ == "__main__":
